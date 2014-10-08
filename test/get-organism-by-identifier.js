5d170504c93cbf11dfa98f9de5ca428869b6cb03
Anders Riutta <git@andersriutta.com>
2014-10-08 16:05:18 -0700

    very messy, but a sample streaming demo is now working for getting organism by identifier

--- test/get-organism-by-identifier.js
+++ test/get-organism-by-identifier.js
@@ -1,7 +1,6 @@
 var bridgedb = require('../index.js');
 
 bridgedb.getOrganismByIdentifier('http://identifiers.org/ncbigene/4292', function(err, organism) {
-  console.log('organism by identifier');
-  console.log(JSON.stringify(organism, null, '\t'));
+  console.log('organism by identifier1');
+  //console.log(JSON.stringify(organism, null, '\t'));
 });
-
---
+++ test/playground.js
@@ -0,0 +1,25 @@
+var bridgedb = require('../index.js');
+var _ = require('lodash');
+var bridgedb1 = bridgedb();
+var bridgedb2 = bridgedb();
+
+//*
+bridgedb1.getOrganismByIdentifier('http://identifiers.org/ncbigene/4292', function(err, organism) {
+  console.log('organism by identifier');
+  console.log(JSON.stringify(organism, null, '\t'));
+});
+//*/
+
+/*
+bridgedb2.getOrganismByIdentifier('http://identifiers.org/ncbigene/103', function(err, organism) {
+  console.log('organism by identifier');
+  console.log(JSON.stringify(organism, null, '\t'));
+});
+//*/
+
+/*
+bridgedb2.getOrganismByIdentifier('http://identifiers.org/ncbigene/103', function(err, organism) {
+  console.log('organism by identifier2');
+  //console.log(JSON.stringify(organism, null, '\t'));
+});
+//*/
