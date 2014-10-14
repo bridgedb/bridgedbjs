c1cf967e65ea7d3471a9fc9bd0b18663edef9011
Anders Riutta <git@andersriutta.com>
2014-10-13 20:19:59 -0700

    Multi-instance appears to be working with streaming. Still needs some clean-up, but it appears we can runOnce and also runOncePerInstance

--- test/playground.js
+++ test/playground.js
@@ -1,19 +1,28 @@
 var Bridgedb = require('../index.js');
 var _ = require('lodash');
-var bridgedb1 = Bridgedb();
-bridgedb1.init({urlStub: 'pointer.ucsf.edu/d3/r/data-sources/bridgedb.php'});
-var bridgedb2 = Bridgedb();
+var bridgedb1 = Bridgedb({urlStub: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb.php'});
+  console.log('bridgedb1');
+  console.log(bridgedb1);
+var bridgedb2 = Bridgedb({urlStub: 'http://webservice.bridgedb.org'});
 //*
-bridgedb1.getOrganismByIdentifier('http://identifiers.org/ncbigene/4292', function(err, organism) {
+bridgedb1.xref.getOrganismByIdentifier('http://identifiers.org/ncbigene/4292', function(err, organism) {
   console.log('organism by identifier1');
   console.log(JSON.stringify(organism, null, '\t'));
+  console.log('bridgedb1a');
+  console.log(bridgedb1);
+  console.log('bridgedb2a');
+  console.log(bridgedb2);
 });
 //*/
 
 //*
-bridgedb2.getOrganismByIdentifier('http://identifiers.org/ncbigene/103', function(err, organism) {
+bridgedb2.xref.getOrganismByIdentifier('http://identifiers.org/ncbigene/103', function(err, organism) {
   console.log('organism by identifier2');
   console.log(JSON.stringify(organism, null, '\t'));
+  console.log('bridgedb1b');
+  console.log(bridgedb1);
+  console.log('bridgedb2b');
+  console.log(bridgedb2);
 });
 //*/
 
