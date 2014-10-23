a84f809685440b3925d2a037ffcb8e1c4aca6525
Anders Riutta <git@andersriutta.com>
2014-10-22 21:45:23 -0700

    We appear to be able to get xrefs for a given entity reference now

--- test/get-organism-by-identifier.js
+++ test/get-organism-by-identifier.js
@@ -6,12 +6,26 @@ var bridgedb1 = Bridgedb({
 });
 var bridgedb2 = Bridgedb();
 
-bridgedb1.organismService.getByIdentifier('http://identifiers.org/ncbigene/4292', function(err, organism) {
+bridgedb1.organismService.getByIri('http://identifiers.org/ncbigene/4292').each(function(organism) {
   console.log('organism by identifier1 should be Homo sapiens');
   console.log(JSON.stringify(organism, null, '\t'));
 });
 
-bridgedb2.organismService.getByIdentifier('http://identifiers.org/ncbigene/174034', function(err, organism) {
+//*
+bridgedb2.organismService.getAvailable().each(function(organisms) {
+  console.log('available organisms');
+  console.log(organisms.length);
+  //console.log(JSON.stringify(organisms, null, '\t'));
+});
+
+bridgedb2.organismService.getAvailable().each(function(organisms) {
+  console.log('available organisms');
+  console.log(organisms.length);
+  //console.log(JSON.stringify(organisms, null, '\t'));
+});
+//*/
+
+bridgedb2.organismService.getByIri('http://identifiers.org/ncbigene/174034').each(function(organism) {
   console.log('organism by identifier2 should be c. elegans');
   console.log(JSON.stringify(organism, null, '\t'));
 });
