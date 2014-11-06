3488ddc89865edcf2d601e6bf6ca143c2d4692d8
Anders Riutta <git@andersriutta.com>
2014-11-06 15:18:03 -0800

    Xrefs formatted for display do not need @context

--- test/get-xrefs.js
+++ test/get-xrefs.js
@@ -4,7 +4,7 @@ var bridgedb1 = Bridgedb({
   datasourcesUrl: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
 });
 
-//*
+/*
 bridgedb1.xrefService.get({
   id: 'http://identifiers.org/ncbigene/4292'
   //bridgedbUri: 'http://webservice.bridgedb.org/Human/xrefs/L/1234'
@@ -14,8 +14,12 @@ bridgedb1.xrefService.get({
 });
 //*/
 
-/*
-bridgedb1.xrefService.get({
+var bridgedb2 = Bridgedb({
+  apiUrlStub: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb.php',
+  datasourcesUrl: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
+});
+//*
+bridgedb2.xrefService.get({
     //id: 'http://identifiers.org/ncbigene/4292'
     bridgedbUri: 'http://webservice.bridgedb.org/Human/xrefs/L/1234'
   },
