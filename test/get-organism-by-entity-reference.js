e4c4a84aeda5872cd7a8fa30c0188caeb1155db0
Anders Riutta <git@andersriutta.com>
2014-11-12 13:52:08 -0800

    Added createStream method for organism service

--- test/get-organism-by-entity-reference.js
+++ test/get-organism-by-entity-reference.js
@@ -11,21 +11,15 @@ var bridgedb2 = Bridgedb({
 });
 
 //*
-bridgedb1.organismService.getByIri('http://identifiers.org/ncbigene/4292').each(function(organism) {
-  console.log('identifier1 organism name should be Homo sapiens');
+bridgedb1.entityReferenceService.enrich('http://identifiers.org/ncbigene/4292')
+.flatMap(bridgedb1.organismService.getByEntityReference)
+.each(function(organism) {
+  console.log('organism name should be Homo sapiens');
   console.log(JSON.stringify(organism, null, '\t'));
 });
 //*/
 
-/*
-bridgedb2.organismService.getAvailable().each(function(organisms) {
-  console.log('available organisms');
-  console.log(organisms.length);
-  //console.log(JSON.stringify(organisms, null, '\t'));
-});
-//*/
-
-/*
+//*
 bridgedb2.organismService.getAvailable().each(function(organisms) {
   console.log('available organisms');
   console.log(organisms.length);
@@ -34,15 +28,28 @@ bridgedb2.organismService.getAvailable().each(function(organisms) {
 //*/
 
 //*
-bridgedb2.organismService.getByIri('http://identifiers.org/ncbigene/174034').each(function(organism) {
-  console.log('identifier2 organism name should be Caenorhabditis elegans');
+bridgedb2.organismService.getAvailable().each(function(organisms) {
+  console.log('available organisms');
+  console.log(organisms.length);
+  //console.log(JSON.stringify(organisms, null, '\t'));
+});
+//*/
+
+//*
+bridgedb2.entityReferenceService.enrich('http://identifiers.org/ncbigene/174034')
+.flatMap(bridgedb2.organismService.getByEntityReference)
+.each(function(organism) {
+  console.log('organism name should be Caenorhabditis elegans');
   console.log(JSON.stringify(organism, null, '\t'));
 });
 //*/
 
 //*
-bridgedb2.organismService.getByIri('http://identifiers.org/ncbigene/174034').each(function(organism) {
-  console.log('identifier2 organism name should be Caenorhabditis elegans (2nd time here)');
+bridgedb2.entityReferenceService.enrich('http://identifiers.org/ncbigene/174034',
+    {sources:['DatabaseMetadata']})
+.pipe(bridgedb2.organismService.createEntityReferenceToOrganismTransformationStream())
+.each(function(organism) {
+  console.log('organism name should be Caenorhabditis elegans (second time)');
   console.log(JSON.stringify(organism, null, '\t'));
 });
 //*/
