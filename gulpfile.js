04645ae4150ad953a4894a67908bc6f902e8b552
Anders Riutta <git@andersriutta.com>
2014-11-13 17:23:55 -0800

    Build and update docs.

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var browserify = require('gulp-browserify');
 var jsdoc = require('gulp-jsdoc');
-var jsDocOptions = require('./docs/conf.json');
+var jsDocOptions = require('./jsdoc-conf.json');
 var uglify = require('gulp-uglify');
 
 // Basic usage
@@ -15,14 +15,14 @@ gulp.task('build', function() {
     .pipe(uglify())
     .pipe(gulp.dest('./dist'));
 
-//I don't know whether gulp-jsdoc is using conf.json.
-//The following command works from the command line:
-//jsdoc -t './node_modules/jaguarjs-jsdoc/' -c './docs/conf.json' './lib/' -r './README.md' -d './docs/'
-
+  /*
+  //I don't know whether gulp-jsdoc is using conf.json.
+  //The following command works from the command line:
+  //jsdoc -t './node_modules/jaguarjs-jsdoc/' -c './jsdoc-conf.json' './lib/' -r './README.md' -d './docs/'
   gulp.src(['./lib/*.js', 'README.md'])
-  //gulp.src(['./lib/*.js'])
     .pipe(jsdoc.parser())
     .pipe(jsdoc.generator('./docs', {
       path: './node_modules/jaguarjs-jsdoc/'
     }, jsDocOptions));
+  //*/
 });
