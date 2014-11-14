4b4dfc85c89b71298f76153e38cbdc933521d61f
Anders Riutta <git@andersriutta.com>
2014-11-13 16:08:16 -0800

    Clean up. Add streaming option for enriching entity references.

--- README.md
+++ README.md
@@ -3,4 +3,11 @@ bridgedbjs
 
 JS client for BridgeDB.org id mapping framework webservices
 
-[Documentation](https://bridgedb.github.io/bridgedbjs/docs/Bridgedb.html)
+[API Documentation](https://bridgedb.github.io/bridgedbjs/docs/Bridgedb.html)
+
+To use with Java, look at [Nashorn](http://openjdk.java.net/projects/nashorn/), [Rhino](https://developer.mozilla.org/en-US/docs/Mozilla/Projects/Rhino) or [DynJS](http://dynjs.org/).
+
+Nashorn tutorials:
+* http://www.oracle.com/technetwork/articles/java/jf14-nashorn-2126515.html
+* http://winterbe.com/posts/2014/04/05/java8-nashorn-tutorial/
+* https://www.youtube.com/watch?v=Cxyg22C5gcw
--- test/get-organism-by-entity-reference.js
+++ test/get-organism-by-entity-reference.js
@@ -1,13 +1,17 @@
-var Bridgedb = require('../index.js');
 var _ = require('lodash');
+var Bridgedb = require('../index.js');
+var highland = require('highland');
+
 var bridgedb1 = Bridgedb({
   apiUrlStub: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb.php',
-  datasourcesUrl: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
+  datasourcesUrl:
+    'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
 });
 
 var bridgedb2 = Bridgedb({
   apiUrlStub: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb.php',
-  datasourcesUrl: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
+  datasourcesUrl:
+    'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
 });
 
 //*
@@ -36,7 +40,8 @@ bridgedb2.organismService.getAvailable().each(function(organisms) {
 //*/
 
 //*
-bridgedb2.entityReferenceService.enrich('http://identifiers.org/ncbigene/174034')
+bridgedb2.entityReferenceService.enrich(
+    'http://identifiers.org/ncbigene/174034')
 .flatMap(bridgedb2.organismService.getByEntityReference)
 .each(function(organism) {
   console.log('organism name should be Caenorhabditis elegans');
@@ -45,9 +50,17 @@ bridgedb2.entityReferenceService.enrich('http://identifiers.org/ncbigene/174034'
 //*/
 
 //*
-bridgedb2.entityReferenceService.enrich('http://identifiers.org/ncbigene/174034',
-    {sources:['DatabaseMetadata']})
-.pipe(bridgedb2.organismService.createEntityReferenceToOrganismTransformationStream())
+highland([
+  'http://identifiers.org/ncbigene/174034'
+])
+.pipe(bridgedb2.entityReferenceService.createEnrichmentStream(
+  {
+    databaseMetadata: true
+  }
+))
+.pipe(
+bridgedb2.organismService.createEntityReferenceToOrganismTransformationStream()
+)
 .each(function(organism) {
   console.log('organism name should be Caenorhabditis elegans (second time)');
   console.log(JSON.stringify(organism, null, '\t'));
