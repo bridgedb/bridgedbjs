fbce968df2c4bb53ce6d8ba934c5808b9d7f94a8
Anders Riutta <git@andersriutta.com>
2014-11-24 17:17:55 -0800

    build

--- gulpfile.js
+++ gulpfile.js
@@ -173,9 +173,7 @@ gulp.task('get-version-type', ['verify-git-status'], function(callback) {
 
 // publish to github repo, github pages and npm.
 gulp.task('publish', ['bump'], function publish(callback) {
-  highland(createGitTagStream('v' + newPackageJson.version,
-          'Version ' + newPackageJson.version))
-  .pipe(createGitPushStream('origin', 'master'))
+  highland(createGitPushStream('origin', 'master'))
   .pipe(createGitPushStream('origin', 'v' + newPackageJson.version))
   .errors(killStream)
   .head()
