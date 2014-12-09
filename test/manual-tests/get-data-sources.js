d451741f30797902949be1668212ad3823169745
Anders Riutta <git@andersriutta.com>
2014-12-08 19:25:07 -0800

    It is not yet pretty, but I'm able to get the data source based on any key that will identify the data source via data-sources.txt

--- test/manual-tests/get-data-sources.js
+++ test/manual-tests/get-data-sources.js
@@ -6,7 +6,7 @@ var bridgeDb1 = BridgeDb({
     'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
 });
 
-//*
+/*
 bridgeDb1.dataSource.getAll()
 .each(function(dataSource) {
   console.log('Database metadata');
@@ -14,15 +14,13 @@ bridgeDb1.dataSource.getAll()
 });
 //*/
 
-//*
 var bridgeDb2 = BridgeDb({
   apiUrlStub: 'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb.php',
   dataSourcesUrl:
     'http://pointer.ucsf.edu/d3/r/data-sources/bridgedb-datasources.php'
 });
-//*/
 
-//*
+/*
 bridgeDb2.dataSource.getAll()
 .each(function(dataSource) {
   console.log('Database metadata');
@@ -30,10 +28,23 @@ bridgeDb2.dataSource.getAll()
 });
 //*/
 
-//*
+/*
 bridgeDb2.dataSource.getAll()
 .each(function(dataSource) {
   console.log('Database metadata');
+  console.log(JSON.stringify(dataSource, null, '\t'));
+});
+//*/
+
+//bridgeDb2.dataSource.getOne({'db', ['Entrez Gene']})
+//bridgeDb2.dataSource.getOne({'db', 'Entrez Gene'})
+bridgeDb2.dataSource.getOne({
+  'db': ['Entrez Gene'],
+  'identifier': '1234'
+})
+//*/
+.each(function(dataSource) {
+  console.log('returned dataSource1234');
   console.log(JSON.stringify(dataSource, null, '\t'));
 });
 //*/
