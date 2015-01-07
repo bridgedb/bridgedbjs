cb15f18a6c9fdedb0116e021be71f134badae70f
Anders Riutta <git@andersriutta.com>
2015-01-06 16:40:47 -0800

    Add sequence library for gulp. Update build process.

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,12 +1,27 @@
 var gulp = require('gulp');
+var gulpSequence = require('gulp-sequence');
 
-//*
-gulp.task('build', [
+/* Manually commit in master
+ * Build
+ *   Check git status for no changes
+ *   Bump metadata files version
+ *   Sync README with current version
+ *   Build docs / Browserify
+ *   Commit again with message "Bumped to version X.Y.Z. Built"
+ *   Create tag with new version
+ * Publish
+ */
+
+gulp.task('build', gulpSequence('verify-git-status',
+      'bump-metadata-files',
+      'sync-readme-version',
+      ['browserify', 'build-docs'],
+      'bump-git'));
+/*
+gulp.task('build', gulpSequence([
   'browserify',
   'build-docs'
 ], function(callback) {
   return callback();
 });
 //*/
-
-//gulp.task('build', ['browserify']);
