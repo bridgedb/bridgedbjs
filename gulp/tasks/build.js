f2e008f3bdaeac536ef571504c555a2d749bf474
Anders Riutta <git@andersriutta.com>
2015-05-04 15:59:38 -0700

    Minor fixes

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,18 +1,26 @@
 var gulp = require('gulp');
 var gulpSequence = require('gulp-sequence');
 
-/* Manually commit in master
+/* Task Steps
+ * Manually commit in master, then this task will:
  * Build
- *   Check git status for no changes
- *   Bump metadata files version
- *   Sync README with current version
- *   Build docs / Browserify
- *   Commit again with message "Bumped to version X.Y.Z. Built"
- *   Create tag with new version
+ *  - Check git status for no changes
+ *  - Bump metadata files version
+ *  - Sync README with current version
+ *  - Build docs / Browserify
+ *  - Commit again with message "Bumped to version X.Y.Z. Built"
+ *  - Create tag with new version
  * Publish
  */
 
+gulp.task('build', gulpSequence(
+    'browserify',
+    'copy'
+));
+
+/*
 gulp.task('build', gulpSequence('verify-git-status',
       'bump-metadata-files',
       ['browserify', 'build-docs'],
       'sync-git-version'));
+//*/
