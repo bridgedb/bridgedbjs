0b7928532c2d96a0a74823ac9ffede0ebc9bfeff
Anders Riutta <git@andersriutta.com>
2015-05-04 16:14:52 -0700

    Move sync-readme-version into its own file

--- gulp/index.js
+++ gulp/index.js
@@ -127,17 +127,6 @@ gulp.task('bump-metadata-files', ['get-version-type'], function(callback) {
 //*/
 
 //*
-gulp.task('sync-readme-version', function() {
-  return gulp.src('README.md')
-    .pipe(replace({
-      regex: oldPackageJson.version,
-      replace: newPackageJson.version
-    }))
-    .pipe(gulp.dest('./'));
-});
-//*/
-
-//*
 // get version type
 gulp.task('get-version-type', function(callback) {
   highland(createPromptStream({
@@ -241,29 +230,3 @@ gulp.task('verify-git-status', function verifyGitStatus(callback) {
     return callback(null, gitStatusOk);
   });
 });
-
-/*
-function killStream(err, push) {
-  if (_.isString(err)) {
-    // err is not of the JS type "error".
-    err = new Error(err);
-  } else if (_.isPlainObject(err)) {
-    // err is not of the JS type "error".
-    var jsError = new Error(err.msg || err.message || 'Error');
-    _.assign(jsError, err);
-    err = jsError;
-  }
-
-  // Using process.exit is a kludge to stop everything in this case.
-  process.exit(1);
-  // It would seem that Highland could kill the stream by
-  // using some combination of the commented-out options below,
-  // but in reality, at least with this version of Highland,
-  // none of those options stop the stream.
-  // Unless we use process.exit, the stream will continue, e.g.,
-  // git diff below will still run.
-  //stream.destroy();
-  //push(err);
-  //throw err;
-}
-//*/
