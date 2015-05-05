150cd983a6d04b77d8d2b8d3a81f637e158e2859
Anders Riutta <git@andersriutta.com>
2015-05-05 13:12:03 -0700

    Working to correct error where package.json is updated after tag

--- gulp/util/git-streaming.js
+++ gulp/util/git-streaming.js
@@ -6,13 +6,14 @@ var execStreaming = highland.wrapCallback(exec);
 
 module.exports = {
   checkout: highland.wrapCallback(git.checkout),
+  commit: highland.wrapCallback(git.commit),
+  createTag: highland.wrapCallback(git.tag),
   merge: highland.wrapCallback(git.merge),
   push: highland.wrapCallback(git.push),
-  status: highland.wrapCallback(git.status),
-  createTag: highland.wrapCallback(git.tag),
   readTags: execStreaming('git tag')
             .split('\r')
             .filter(function(tag) {
               return tag !== null && typeof tag !== 'undefined' && tag !== '';
-            })
+            }),
+  status: highland.wrapCallback(git.status)
 };
