48ab318ea061994ecbfe8da462ac88b6b6360389
Anders Riutta <git@andersriutta.com>
2015-05-05 13:03:35 -0700

    Working to correct error where package.json is updated after tag

---
+++ gulp/tasks/commit-after-build.js
@@ -0,0 +1,24 @@
+var fs = require('fs');
+var git = require('gulp-git');
+var gitStreaming = require('../util/git-streaming.js');
+var gulp = require('gulp');
+var highland = require('highland');
+var metadataFilePaths = require('../util/metadata-file-paths.json');
+
+gulp.task('commit-after-build', function commitAfterBuild(callback) {
+  var package = JSON.parse(fs.readFileSync('package.json'));
+  var version = package.version;
+
+  gulp.src(['./dist/*',
+            './docs/*',
+            'README.md']
+            .concat(metadataFilePaths)
+  )
+  .pipe(highland.pipeline())
+  .through(git.add())
+  .through(gitStreaming.commit('Built and bumped version to ' + version + '.'))
+  .last()
+  .each(function() {
+    return callback();
+  });
+});
