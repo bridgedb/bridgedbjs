f2992bcee7ac6d976b013196a1b678586bc6f32f
Anders Riutta <git@andersriutta.com>
2015-05-05 12:48:44 -0700

    Working to correct error where package.json is updated after tag

--- gulp/tasks/bump-git-tag.js
+++ gulp/tasks/bump-git-tag.js
@@ -28,10 +28,12 @@ gulp.task('bump-git-tag', function bumpGitTag(callback) {
               .concat(metadataFilePaths)
     )
     .pipe(highland.pipeline())
+    /*
     .through(git.add())
     .through(git.commit('Built and bumped version to ' + version + '.'))
     .debounce(1000)
     .last()
+    //*/
     .through(gitStreaming.createTag(version,
             'Version ' + version))
     .last()
