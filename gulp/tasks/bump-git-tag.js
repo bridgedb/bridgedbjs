24448f2157640e7b636146efedad6787146a835e
Anders Riutta <git@andersriutta.com>
2015-05-04 17:29:38 -0700

    Further update build process to allow for updating git tag.

---
+++ gulp/tasks/bump-git-tag.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var git = require('gulp-git');
+var gitStreaming = require('../util/git-streaming.js');
+var gulp = require('gulp');
+var highland = require('highland');
+var metadataFilePaths = require('../util/metadata-file-paths.json');
+
+gulp.task('bump-git-tag', function bumpGitTag(callback) {
+  var package = JSON.parse(fs.readFileSync('package.json'));
+  var version = package.version;
+
+  gitStreaming.readTags
+  .reduce(false, function checkTagExists(accumulator, tag) {
+    if (accumulator || (tag === version)) {
+      return true;
+    }
+
+    return false;
+  })
+  .each(function(tagExists) {
+    if (tagExists) {
+      return callback();
+    }
+
+    gulp.src(['./dist/*',
+              './docs/*',
+              'README.md']
+              .concat(metadataFilePaths)
+    )
+    .pipe(git.add())
+    .pipe(git.commit('Built and bumped version to ' + version + '.'))
+    .pipe(gitStreaming.createTag(version,
+            'Version ' + version))
+    .last()
+    .each(function() {
+      return callback();
+    });
+  });
+});
