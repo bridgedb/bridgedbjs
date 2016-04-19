4297673d8da45093067b3f7a0feb2ea5e65d8f70
Anders Riutta <git@andersriutta.com>
2016-04-19 12:04:49 -0700

    Now rendering datasource selector with yolk and also passing initial test

---
+++ karma.conf.js
@@ -0,0 +1,55 @@
+var bridgeDbDefaultConfig = require('./lib/config.js');
+//require('./test/mockserver.js');
+
+module.exports = function(config) {
+  var options = {
+    basePath: '',
+    frameworks: ['mocha', 'chai', 'browserify'],
+    browsers: [
+      'Chrome',
+      'Firefox'
+    ],
+    files: ['test/unit/ui-components/**/*.js'],
+    preprocessors: {
+      'test/unit/ui-components/**/*.js': 'browserify'
+    },
+    browserify: {
+      debug: true,
+      transform: ['brfs']
+    },
+    colors: true,
+    reporters: [
+      'coverage',
+      'mocha',
+    ],
+    browserDisconnectTimeout: 60000,
+    browserNoActivityTimeout: 60000,
+    client: {
+      mocha: {
+        timeout: 59000,
+      },
+    },
+  };
+
+  process.env.MOCKSERVER_PORT = 4522;
+  var proxies = {};
+  proxies[bridgeDbDefaultConfig.baseIri] = {
+    target: 'http://localhost:' + process.env.MOCKSERVER_PORT + '/',
+    changeOrigin: true
+  };
+  proxies[bridgeDbDefaultConfig.datasetsMetadataIri] = {
+    target: 'http://localhost:' + process.env.MOCKSERVER_PORT + '/datasources.txt',
+    changeOrigin: true
+  };
+  options.proxies = proxies;
+
+  if (process.env.CIRCLECI) {
+    options.singleRun = true;
+  }
+
+  if (process.env.GROWL) {
+    options.reporters.push('growl');
+  }
+
+  config.set(options);
+};
