bdad311bee35f78b6b1114003cfe8ca085e2b5c9
Anders Riutta <git@andersriutta.com>
2016-11-08 14:54:11 -0800

    Clean up.

--- src/ui/react-demo.tsx
+++ src/ui/react-demo.tsx
@@ -4,6 +4,9 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
 ReactDOM.render(
-  <h1>Hello, world2!</h1>,
+	<div>
+		<h1>Hello, world!</h1>
+		<p>This is React.</p>
+	</div>,
   document.querySelector('body')
 );
